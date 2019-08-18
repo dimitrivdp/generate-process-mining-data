ce6c6c9a43fb6cac37bb83809d0067b7c2b43cfe
Arjen <arjen@bos4.nl>
2019-08-18 10:26:45 +0200

    Incorporated df_steps as process_description into Case

--- generate_data.py
+++ generate_data.py
@@ -19,13 +19,11 @@ END = "END"
 DURATION_UNIT = "minutes"
 
 
-def parse_argv(input_filename="examples/process_with_loops.xlsx", approx_rows=100):
+def parse_argv(input_filename=None, approx_rows=None):
     # argv = argument vector
     n = len(sys.argv)
-    if n > 1:
-        input_filename = sys.argv[1]
-    if n > 2:
-        approx_rows = int(sys.argv[2])
+    input_filename = sys.argv[1] if n > 1 else input_filename
+    approx_rows = int(sys.argv[2]) if n > 2 else approx_rows
     return (input_filename, approx_rows)
 
 
@@ -42,15 +40,25 @@ def inspect_df(df, n=5):
 
 
 def read_input_file(input_filename):
-    df_steps = pd.read_excel(input_filename, sheet_name="steps", index_col="step_id")
+    # Required columns: step_id, duration
+    # Optional columns: step_name, wait_time, duration_outliers, wait_time_outliers and other custom columns
+    excel_steps = pd.read_excel(input_filename, sheet_name="steps", index_col="step_id")
 
-    # TODO: Add optional columns: wait_time, duration_outliers, wait_time_outliers
+    # Missing wait_time, duration_outliers and wait_time_outliers are added because the script relies on them
+    keys = list(excel_steps.columns)
+    if "wait_time" not in keys:
+        excel_steps["wait_time"] = 0
+    if "duration_outliers" not in keys:
+        excel_steps["duration_outliers"] = False
+    if "wait_time_outliers" not in keys:
+        excel_steps["wait_time_outliers"] = False
 
     # Convert the time columns to a timedelta
-    df_steps.duration = df_steps.duration.apply(to_timedelta)
-    df_steps.wait_time = df_steps.wait_time.apply(to_timedelta)
+    excel_steps.duration = excel_steps.duration.apply(to_timedelta)
+    excel_steps.wait_time = excel_steps.wait_time.apply(to_timedelta)
 
-    df_flow = pd.read_excel(
+    # Flow sheet has a fixed layout: step_id, next_step_id and probability
+    excel_flow = pd.read_excel(
         input_filename, sheet_name="process_flow", index_col="step_id"
     )
 
@@ -59,13 +67,13 @@ def read_input_file(input_filename):
     # Datatypes: timedeltas and bools
     # All probability columns: negative probability can not exist
     # All probability columns: all rows must sum to 1, except row N, must all equal 0
-    return (df_steps, df_flow)
+    return (excel_steps, excel_flow)
 
 
-def merge_flow_into_steps(df_steps, df_flow):
+def merge_flow_into_steps(excel_steps, excel_flow):
     # Group the flow dataframe into lists for easy usage later on with random.choices()
-    df_flow_grouped = (
-        df_flow.groupby(by="step_id")
+    excel_flow_grouped = (
+        excel_flow.groupby(by="step_id")
         .agg(list)
         .rename(
             columns={
@@ -75,8 +83,10 @@ def merge_flow_into_steps(df_steps, df_flow):
         )
     )
     # Join the step dataframe with next possible steps info
-    df_steps = pd.merge(df_steps, df_flow_grouped, how="left", on="step_id")
-    return df_steps
+    process_description = pd.merge(
+        excel_steps, excel_flow_grouped, how="left", on="step_id"
+    )
+    return process_description
 
 
 def randomize_duration(duration, has_outliers=False):
@@ -100,10 +110,11 @@ class Case:
     """
 
     # Initiate attributes of a new instance of Case
-    def __init__(self):
+    def __init__(self, process_description):
         self.uuid = str(uuid4())
         self.clock = datetime.now()  # TODO: Randomize this
         self.done = False
+        self.process_description = process_description
 
         # Initiate current state with some values in a dictionary
         # Note: Don't try slicing the last row of self.history. You won't be able to set values properly. Read http://pandas.pydata.org/pandas-docs/stable/user_guide/indexing.html#why-does-assignment-fail-when-using-chained-indexing
@@ -112,29 +123,29 @@ class Case:
             "step_id": START,
             "start_time": self.clock,
             "end_time": None,
+            # And insert the values of the process description for this step
+            **self.process_description.loc[START].to_dict(),
         }
 
-        # Also keep track of all previous steps in a dataframe
-        self.history = pd.DataFrame()
+        # Also keep track of all previous steps in a list
+        self.history = []
 
     def end_current_step(self):
-        # Proceed to the end of the step
+        # Execute the step, so time will pass
         self.clock += randomize_duration(
-            df_steps.loc[self.current["step_id"], "duration"],
-            df_steps.loc[self.current["step_id"], "duration_outliers"],
+            self.current["duration"], self.current["duration_outliers"]
         )
 
-        # Now is the end time of the current step
+        # Register the end time of the step
         self.current["end_time"] = self.clock
 
         # This step is completed so write the current status to history
-        self.history = self.history.append(self.current, ignore_index=True)
+        self.history.append(self.current)
 
     def wait_after_step(self):
         # Add some wait time, that's all for now
         self.clock += randomize_duration(
-            df_steps.loc[self.current["step_id"], "wait_time"],
-            df_steps.loc[self.current["step_id"], "wait_time_outliers"],
+            self.current["wait_time"], self.current["wait_time_outliers"]
         )
 
     def go_to_next_step(self):
@@ -151,8 +162,8 @@ class Case:
 
         # And choose (random) the next step
         next_step_id = choices(
-            df_steps.loc[self.current["step_id"], "next_possible_steps_id"],
-            df_steps.loc[self.current["step_id"], "next_possible_steps_probability"],
+            self.current["next_possible_steps_id"],
+            self.current["next_possible_steps_probability"],
         )[0]
 
         # Actually start the next step by creating a new current status
@@ -161,6 +172,8 @@ class Case:
             "step_id": next_step_id,
             "start_time": self.clock,
             "end_time": None,
+            # Again, also add process description for the upcoming step
+            **self.process_description.loc[next_step_id].to_dict(),
         }
 
     def walk_through_process(self):
@@ -191,15 +204,15 @@ class Case:
 
 def post_processing(df):
     # Join step names
-    df = df.merge(
-        df_steps.loc[:, "step_name"], how="left", left_on="step_id", right_index=True
-    )
+    # df = df.merge(
+    #    excel_steps.loc[:, "step_name"], how="left", left_on="step_id", right_index=True
+    # )
 
     # Remove START and END
     df = df[(df.step_id != START) & (df.step_id != END)]
 
-    print("Shape of output table: ", end="")
-    inspect_df(df, n=10)
+    #print("Shape of output table: ", end="")
+    #inspect_df(df, n=10)
     return df
 
 
@@ -208,25 +221,27 @@ if __name__ == "__main__":
     timer = time.time()
 
     # Parse command line arguments (input filename and approximate number of rows)
-    (input_filename, approx_rows) = parse_argv()
+    (input_filename, approx_rows) = parse_argv(
+        "examples/process_multiple_employees.xlsx", 100
+    )
 
     # Read and process the sheets from the input Excel
     print("Reading input from:", input_filename)
-    (df_steps, df_flow) = read_input_file(input_filename)
-    df_steps = merge_flow_into_steps(df_steps, df_flow)
+    (excel_steps, excel_flow) = read_input_file(input_filename)
+    process_description = merge_flow_into_steps(excel_steps, excel_flow)
 
     # Note: If you are new to this script, definitely take a look at this dataframe
-    inspect_df(df_steps, n=10)
+    #inspect_df(process_description, n=10)
 
     # Generate random cases until the dataframe is full
     df = pd.DataFrame()
     print("Processing row:")
     while len(df) < approx_rows:
         # Start a case and walk through the process
-        case = Case()
+        case = Case(process_description)
         case.walk_through_process()
 
-        # Extract the history and append to the main dataframe
+        # Append the history (completed steps) of this case to our dataset
         df = df.append(case.history)
         print("\r" + str(len(df)), end="")
 
@@ -236,8 +251,10 @@ if __name__ == "__main__":
     print("\nDone in: %.1f sec" % (time.time() - timer))
 
 
+    inspect_df(df)
+
     # Save to file
     makedirs("./output", exist_ok=True)
     # TODO: use inputfile name .csv
-    #df.to_csv("./output/process_data.csv", index=False)
+    # df.to_csv("./output/process_data.csv", index=False)
     print("Dataset saved in: output/process_data.csv")
